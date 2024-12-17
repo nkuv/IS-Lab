64a0b04b1c772064834d9076c3c4558a5cb53fd7
Nandhu <nandhukrishna1217@gmail.com>
2024-12-17 23:11:19 +0530

    new file:   CeaserCipher.py

---
+++ CeaserCipher.py
@@ -0,0 +1,15 @@
+def caesar_cipher(text,shift):
+    encrypted = ""
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            shifted = (ord(char) + shift - base) % 26
+            encrypted += chr(base + shifted)
+        else:
+            encrypted += char
+    return encrypted
+
+plaintext = input("Enter a plaintext: ")
+shift = 3
+ciphertext = caesar_cipher(plaintext,shift)
+print("Encrypted Text:", ciphertext)
