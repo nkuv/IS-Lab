1b81b4bff2500860bad93477e048baf97fbb0271
Nandhu <nandhukrishna1217@gmail.com>
2024-12-17 23:41:47 +0530

    Updated: CeaserCipher.py

--- CeaserCipher.py
+++ CeaserCipher.py
@@ -1,15 +1,19 @@
-def caesar_encrypt(text,shift):
+def ceaser_encrypt(text,key):
     encrypted = ""
     for char in text:
         if char.isalpha():
             base = ord('A') if char.isupper() else ord('a')
-            shifted = (ord(char) + shift - base) % 26
+            shifted = (ord(char) + key - base) % 26
             encrypted += chr(base + shifted)
         else:
             encrypted += char
     return encrypted
 
 plaintext = input("Enter a plaintext: ")
-shift = 3
-ciphertext = caesar_encrypt(plaintext,shift)
-print("Encrypted Text:", ciphertext)
+key = 3
+ciphertext = ceaser_encrypt(plaintext,key)
+print("Encrypted Text:",ciphertext)
+
+ciphertext = input("Enter a ciphertext: ")
+plaintext = ceaser_encrypt(ciphertext,-key)
+print("Decrypted Text:",plaintext)
