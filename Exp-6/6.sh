f2924e3227768ac5a77c2534d447b09705192609
Nandhu <nandhukrishna1217@gmail.com>
2025-03-10 23:01:37 +0530

    Update

--- Exp-6/6.sh
+++ Exp-6/6.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 
-# Demonstrate fork, exec, getpid, exit, wait, close, stat, opendir, readdir
-
-# 1. fork + exec: Run a command in a child process
 echo "Parent PID: $$"
-( echo "Child PID: $BASHPID"; exec ls / ) &  # Child forks, then execs 'ls /'
-child_pid=$!
-echo "Child process started with PID: $child_pid"
+( echo "Child PID: $BASHPID"; exec ls / ) & child_pid=$!
+echo "Child started: $child_pid"
 
-# 2. wait: Wait for child process to finish
-wait $child_pid
-echo "Child process $child_pid exited."
-
-# 3. getpid: Show current PID
-echo "Current PID: $$"
-
-# 4. stat: Get file status
-echo "Stat of /etc/passwd:"
+wait $child_pid && echo "Child $child_pid exited."
 stat /etc/passwd
 
-# 5. opendir + readdir: List directory contents
-echo "Files in /tmp:"
-for file in /tmp/*; do
-    echo "$file"
-done
+echo -e "\nFiles in /tmp:"; ls /tmp
 
-# 6. close: Open and close a file descriptor
-exec 3>test.txt        # Open FD 3
-echo "Hello World" >&3  # Write to FD 3
-exec 3>&-              # Close FD 3
+exec 3>test.txt && echo "Hello World" >&3 && exec 3>&-
 echo "Closed FD 3."
-
-# 7. exit
 exit 0
--- Exp-7/7.sh
+++ Exp-7/7.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 exec 3< input.txt
-exec 4> output.txt             
+exec 4> output.txt  
+
 while IFS= read -r line <&3; 
   do echo "$line" >&4; 
 done
+
 exec 3<&- 4>&-             
