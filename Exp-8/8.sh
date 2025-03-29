4665380eabd651edb7830126eb361c9e8c7bfe16
Nandhu <nandhukrishna1217@gmail.com>
2025-03-29 14:51:32 +0000

    Added exp 8

---
+++ Exp-8/8.sh
@@ -0,0 +1,17 @@
+#List out all users
+cat /etc/passwd | cut -d: -f1
+
+#Add a user
+sudo useradd mec1
+
+#Assign password to user
+sudo passwd mec1
+
+#Access user configuration file
+cat /etc/passwd
+
+#Change user login name of a user
+sudo usermod -l mec2 mec1
+
+#Delete a user name
+sudo userdel mec2
